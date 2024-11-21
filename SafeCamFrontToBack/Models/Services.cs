4f87c1a3a93b1044d87eeed03ea72b3b971ad4a7
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-21 05:49:14 +0400

    add Services model with title, description, and about properties

---
+++ SafeCamFrontToBack/Models/Services.cs
@@ -0,0 +1,8 @@
+﻿namespace SafeCamFrontToBack.Models;
+
+public class Services
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string About { get; set; }
+}
