4c82371fccdd7ac8521596956be1ad33410d4ce0
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-21 05:39:37 +0400

    implement css components for improved functionality

---
+++ SafeCamFrontToBack/Models/TeamMember.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeCamFrontToBack.Models;
+
+public class TeamMember
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public string ImgUrl { get; set; }
+}
