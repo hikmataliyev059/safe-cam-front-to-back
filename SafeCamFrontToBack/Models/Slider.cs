34f004c4b22b9227eea1a97acb6b030341f6caa5
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-21 05:49:48 +0400

    Add Slider model with id, title, description, and url properties

---
+++ SafeCamFrontToBack/Models/Slider.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeCamFrontToBack.Models;
+
+public class Slider
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImgUrl { get; set; }
+}
