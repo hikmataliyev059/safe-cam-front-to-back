6025b67737aed464bfcf808ebf6aba9570898ab4
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-21 05:52:10 +0400

    add AppDbContext to handle database operations for Sliders and TeamMembers

---
+++ SafeCamFrontToBack/DAL/AppDbConttext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SafeCamFrontToBack.Models;
+
+namespace SafeCamFrontToBack.DAL;
+
+public class AppDbConttext : DbContext
+{
+    public AppDbConttext(DbContextOptions<AppDbConttext> options) : base(options)
+    {
+    }
+
+    public DbSet<Slider> Sliders { get; set; }
+    
+    public DbSet<TeamMember> TeamMembers { get; set; }
+}
