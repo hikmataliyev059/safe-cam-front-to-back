cc2e553e7982d38d93ed54cd00fd867d89a8ba38
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-21 05:53:29 +0400

    set up dependency injection and database configuration

---
+++ SafeCamFrontToBack/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SafeCamFrontToBack.DAL;
+
+namespace SafeCamFrontToBack;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDbContext<AppDbConttext>(opt =>
+            opt.UseSqlServer(
+                "Server=localhost,51433;Database=safe_cam; " +
+                "User ID=sa;Password=StrongP@ssw0rd123;TrustServerCertificate=True")
+        );
+
+        var app = builder.Build();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
