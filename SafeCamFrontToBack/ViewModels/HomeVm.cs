7dbd106b70c74cdc56e6eef975af9d0497a04e78
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-21 05:48:42 +0400

    create to pass data for sliders, services, and teamMembers

---
+++ SafeCamFrontToBack/ViewModels/HomeVm.cs
@@ -0,0 +1,10 @@
+﻿using SafeCamFrontToBack.Models;
+
+namespace SafeCamFrontToBack.ViewModels;
+
+public class HomeVm
+{
+    public List<Slider> Sliders { get; set; }
+    public List<Services> Services { get; set; }
+    public List<TeamMember> TeamMembers { get; set; }
+}
